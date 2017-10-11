37b3553fd3888e33b3a83c460bb1283c8628ddce
admin.dakota <admin.dakota@HU130H005.Blackburn.College>
2017-10-10 21:14:00 -0500

    Moved Files

--- ClientRewrite.java
+++ ClientRewrite.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cs255hw2.shared;
+
 
 import java.io.*;
 import java.net.*;
--- SenderRewrite.java
+++ SenderRewrite.java
@@ -3,9 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cs255hw2.Sender;
 
-import cs255hw2.shared.*;
+
 import java.io.IOException;
 
 /**
